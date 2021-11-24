9a100b902c525956db2e584699ccb63baabb2840
Eduardo Barros <eduardo.araujo@outlook.com.br>
2021-11-24 10:16:14 -0300

    add methods

--- src/employees/Employee.java
+++ src/employees/Employee.java
@@ -1,24 +1,87 @@
-package employess;
+package employees;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Scanner;
 
 public abstract class Employee {
-    public String name;
+    Scanner scan = new Scanner(System.in);
+    protected String name;
+    protected String employeeType;
+    protected int age;
+    protected int cpf;
+    protected boolean syndicate;
+    protected String address;
+    protected String paymentMethod;
 
-    public Employee() {
-    }
-
-    public Employee(String name) {
-        this.name = name;
+    public void setName(String name) {
+        this.name = name;  // Retirar \n
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCpf(int cpf) {
+        // Check cpf
+        this.cpf = cpf;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setSyndicate(@NotNull String syndicate) {
+        this.syndicate = syndicate.equals("Y");
+    }
+
+    public boolean getSyndicate() {
+        return this.syndicate;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+        // check only first line input
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getPaymentMethod() {
+        return this.paymentMethod;
+    }
+
+    public void setEmployeeType(String employeeType) {
+        this.employeeType = employeeType;
+    }
+
+    public String getEmployeeType() {
+        return this.employeeType;
     }
 
     public abstract String definePayment(String paymentType);
+
+    public void printEmployee() {
+        System.out.println("CPF: " + this.getCpf() + " Name: " + this.getName() +
+                "\nAge: " + this.getAge() +
+                "\nAddress: " + this.getAddress() +
+                "\nSyndicate: " + this.getSyndicate() +
+                "\nType: " + this.getEmployeeType() +
+                "\nPayment method: " + this.getPaymentMethod());
+    }
 
     @Override
     public String toString() {
