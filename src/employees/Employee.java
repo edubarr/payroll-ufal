c48970dbada4ed4bdd03e24cadc1fbae9f0c0052
Eduardo Barros <eduardo.araujo@outlook.com.br>
2021-11-24 09:19:30 -0300

    add employees initial files

---
+++ src/employees/Employee.java
@@ -0,0 +1,28 @@
+package employess;
+
+public abstract class Employee {
+    public String name;
+
+    public Employee() {
+    }
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract String definePayment(String paymentType);
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return this.getName();
+    }
+}
---
+++ src/employees/Hourly.java
@@ -0,0 +1,29 @@
+package employess;
+
+public class Hourly extends Employee {
+
+    private double workingHours;
+
+    public Hourly(String name, double workingHours) {
+        super(name);
+        this.workingHours = workingHours;
+    }
+
+    public double getWorkingHours() {
+        return workingHours;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return this.getWorkingHours() + ":" +
+                super.getName();
+    }
+
+    @Override
+    public String definePayment(String paymentType) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
